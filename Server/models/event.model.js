1a8c281518d13e08a336c66a9857112d828341d3
apacheraider <apacheraider123@gmail.com>
2025-02-19 21:31:37 -0700

    model is done

---
+++ Server/models/event.model.js
@@ -0,0 +1,41 @@
+import { model, Schema } from "mongoose"
+
+const EventSchema = new Schema({
+    eventType : {
+        type : String,
+        required : [true, "You must pick a type" ],
+        default : "Benefit"
+    },
+    eventName : {
+        type : String,
+        required : [true, "Event must have a name"],
+        minlength : [5, "eventName must be at least 5 characters long."],
+        maxlength : [50, "Event must be no longer than 50 characters."]
+    },
+    eventDateTime : {
+        type : Date,
+        required : [true, "Your event must have a date and time"],
+    },
+    numberOfPeople : {
+        type : Number,
+        required : [true, "How many people will be at your event"],
+        max : [200, "The venue has max occupancy of 200"],
+        default : 0,
+    },
+    image : {
+        type : String,
+
+    },
+    description : {
+        type : String,
+        minlength : [3, "Describe your event with at least 3 characters."],
+        maxlength : [255, "Describe your event in less than 255 characters."],
+
+    },
+
+}, 
+    { timestamps : true } 
+);
+
+const Event = model("Event", EventSchema );
+export default Event;
